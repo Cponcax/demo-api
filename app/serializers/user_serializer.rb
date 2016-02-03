9476f8b58e0030208ef301dbf7714bf26e0a67f3
Rafael <rafael.valencia@503estudio.com>
2016-02-03 16:55:02 -0600

    Change user_serializer.rb

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email
+  attributes :id, :username, :first_name, :last_name, :email, :gender, :birth_date, :bio
 end
