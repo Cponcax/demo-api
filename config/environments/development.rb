0e2c770e8d8af8a605ca650e65f6443cdaf8cfab
Rafael <rafael.valencia@503estudio.com>
2016-02-04 15:48:54 -0600

    Configure sending of emails with mandrill.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -40,4 +37,23 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true 
+  
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true 
+
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    domain: "lvh.me",
+    authentication: :login,
+    enable_starttls_auto: true,
+    user_name: ENV.fetch('mandrill_user'),
+    password: ENV.fetch('mandrill_api_key')
+  }
 end
