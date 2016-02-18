05ceb0e50a700dd704f74eb32f221511875362dd
Luis <luis.castro@503estudio.com>
2016-02-18 09:47:38 -0600

    Adding seed to create channel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+channel = Channel.create([{name: 'canal cuatro', logo:'logo canalcuatro', streaming_url: 'streaming_url_canalcuatro', position: '4'}])
