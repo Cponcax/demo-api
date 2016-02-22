9eb5e8fc9d890c4fedac167ed720db8ff3ee28ab
Alex Ponce <alex.ponce@503estudio.com>
2016-02-22 11:47:38 -0600

    Adding changes in migration for channel and seed

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20160219190945) do
   create_table "channels", force: :cascade do |t|
     t.string   "name"
     t.integer  "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "streaming_url"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "logo_color"
   end
 
