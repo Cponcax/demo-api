e7564056baab0988a89cc9017baf57613b0e0685
Alex Ponce <alex.ponce@503estudio.com>
2016-02-26 09:13:52 -0600

    merge with dvelop

--- app/controllers/v1/channels_controller.rb
+++ app/controllers/v1/channels_controller.rb
@@ -16,28 +16,23 @@ class V1::ChannelsController < V1::BaseController
  api :GET, "/channels/:id/schedules?day=1", "Get list of programs per channel per schedules per day. Where monday is 1 and sunday is 7"
   param_group :error, V1::BaseController
   param :shows, Hash, :desc => "Contains show information.", :required => true do
-    param :channel_id, :number, :required => true
-     param :schedule_id, :number, :required => true
     param :name, String, :desc => "show name" , :required => true
     param :cover, String, :desc => "cover of the ", :required => true
     param :start_time, String, :desc => "start time ", :required => true
-    param :end_time, String, :desc => "end time ", :required => true
-    param :streaming_url, String, :desc => "streaming url for event", :required => true
-    param :date, Date, :desc => "Date of the event", :required => true
   end
   formats ['json']
   example'
-  {
-    "shows":[
+
+{
+  "shows":[
     {
-    "channel_id": 2,
-    "schedule": "tarde",
+    "id": 1,
     "name": "viva la manana",
+    "rating": "PG",
+    "logo": "https://i.ytimg.com/vi/Q0NzALRJifI/maxresdefault.jpg",
     "cover": "https://i.ytimg.com/vi/Q0NzALRJifI/maxresdefault.jpg",
-    "start_time": "2016-02-16T16:10:30.000Z",
-    "end_time": "2016-02-16T18:10:30.000Z",
-    "streaming_url": "streaming event",
-    "date": "2016-02-16"
+    "start_time": "2000-01-01T16:20:00.000Z",
+    "turn": "tarde"
     }
   ]
 }
