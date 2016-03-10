48ba5cd93f3923993e6ece4edeb58dda18992f97
Alex Ponce <alex.ponce@503estudio.com>
2016-03-10 15:00:07 -0600

    Add changes in chanels.rb & events_serializer.rb & show_serializer.rb

--- app/serializers/show_serializer.rb
+++ app/serializers/show_serializer.rb
@@ -1,17 +1,5 @@
 class ShowSerializer < ActiveModel::Serializer
-  attributes :id, :name, :rating, :logo, :datetime, :cover,  :turn
+  attributes :id, :name, :rating, :logo,  :cover
 
-
-
-  def turn
-    object.schedules.first.turn
-  end
-
-  def datetime
-
-    d = object.schedules.first.date
-    t = object.events.first.start_time
-    dt = DateTime.new(d.year, d.month , d.day, t.hour, t.min, t.sec)
-  end
 
 end
