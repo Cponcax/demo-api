808ebe0f80a325924f2b316abd9d8c4176bc818b
Alex Ponce <alex.ponce@503estudio.com>
2016-03-10 15:12:12 -0600

    add timezone in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.time_zone = 'Central America'
+  config.active_record.default_timezone = :local
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
