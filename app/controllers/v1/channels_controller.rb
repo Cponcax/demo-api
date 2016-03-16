cafa9650b9e9a736b34e4f2ee4c642b9e1fb96dc
Alex Ponce <alex.ponce@503estudio.com>
2016-03-16 17:22:00 -0600

    adding changes to change offset

--- app/controllers/v1/channels_controller.rb
+++ app/controllers/v1/channels_controller.rb
@@ -12,67 +12,37 @@ class V1::ChannelsController < V1::BaseController
   end
 
 
- api :GET, "/channels/:id/schedules?day=1", "Get list of programs per channel per schedules per day. Where monday is 1 and sunday is 7"
-
-
-    param :id, Integer, :desc => "id of channel" , :required => true
-    param :day, Integer, :desc => "day of the week  ", :required => true
-
-
-  formats ['json']
-  example'
-
+ api :GET, "/channels/:id/schedules?day=1&date=1458166403", "Get list of programs per channel per schedules per day. Where monday is 1 and sunday is 7"
+    param :id, Integer, :desc => "Id of the channel" , :required => true
+    param :day, Integer, :desc => "Day of the week in number where Mondar is 1 and Sunday is 7  ", :required => true
+    param :date, Integer, :desc => "Date in format Unix timestamp ", :required => true
+    formats ['json']
+    example'
 {
   "Events":[
     {
-    "id": 1,
-    "name": "TCS noticias",
-    "rating": "nc_17",
-    "logo": "/system/shows/logos/1/download_%282%29_original.jpeg?1457453604",
-    "datetime": "2016-03-07T06:14:00.000+00:00",
-    "cover": "/system/shows/covers/1/download_%283%29_original.jpeg?1457453604",
-    "turn": "morning"
+      "id": 17,
+      "channel_id": 1,
+      "name": "Viva la manana",
+      "cover": "https://503development.s3.amazonaws.com/shows/covers/2/download_%285%29_original.jpeg?1457643848",
+      "logo": "https://503development.s3.amazonaws.com/shows/logos/2/hqdefault_original.jpg?1457643848",
+      "rating": "g",
+      "datetime": "2016-03-16T15:00:00.000-06:00"
     }
   ]
+}
   '
-  def channel_shows
-    day =  params[:day]
-    unixdate = params[:date]
-    date = unixdate.to_i
-    date_parse = Time.at(date).to_datetime
-    # date_conver = DateTime.parse(date)
-    #date_current_parse = date_parse.new_offset(DateTime.now.offset)
-    date_current = DateTime.now
-    case
-    when day == "1" && date_parse.hour == date_current.hour
-      @channel.monday
-      render json: @channel.monday, root: "events"
-    when day == "2"
-      @channel.tuesday
-      render json: @channel.tuesday, root: "events"
-    when day == "3"
-      @channel.wednesday
-      render json: @channel.wednesday, root: "events"
-    when day == "4"
-      @channel.thursday
-      render json: @channel.thursday, root: "events"
-    when day == "5"
-      @channel.friday
-      render json: @channel.friday, root: "events"
-    when day == "6"
-      @channel.saturday
-      render json: @channel.saturday, root: "events"
-    when day == "7"
-      @channel.sunday
-      render json: @channel.sunday, root: "events"
-    else
-     render json: @channel.errors, status: 404
+    def channel_shows
+      day =  params[:day]
+      unixdate = params[:date]
+      date = unixdate.to_i
+      date_parse = Time.at(date).to_datetime
+      date_current = DateTime.now
+      @channel.event_with_date(day,date_parse)
+      render json: @channel.event_with_date(day, date_parse), root: "events"
     end
-  end
-
 
   private
-
 
     def set_channel
       @channel = Channel.find(params[:id])
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -10,80 +10,28 @@ class Channel < ActiveRecord::Base
 
   validates :name,  :position, :streaming_url, :logo_color, presence: true
 
-  def monday
+  def event_with_date(day, date_parse)
     shows = []
 
     self.schedules.each do |schedule|
-      if schedule.date.wday == 1 && schedule.date.strftime("%U")== Time.current.strftime("%U")
-      	shows << schedule.events.order('start_time ASC')
+      if schedule.date.wday == day.to_i && schedule.date.strftime("%U")== Time.current.strftime("%U")
+        shows << schedule.events
       end
+      events = schedule.events
+      hour = date_parse.hour
+      wday = schedule.date.wday
+      uwday = date_parse.utc.wday
+
+      filter_events = events.select { |e|
+        uwday == wday && hour == e.start_time.hour
+    puts "hour : "+ hour.to_s
+      puts "hour : "+  e.start_time.hour.to_s
+        puts "day : "+  uwday.to_s
+      }
+      shows << filter_events
     end
     shows.flatten
   end
 
-  def tuesday
-    shows = []
-
-    self.schedules.each do |schedule|
-      if schedule.date.wday == 2 && schedule.date.strftime("%U")== Time.current.strftime("%U")
-        shows << schedule.events.order('start_time ASC')
-      end
-    end
-    shows.flatten
-  end
-
-  def wednesday
-    shows = []
-
-    self.schedules.each do |schedule|
-      if schedule.date.wday == 3 && schedule.date.strftime("%U")== Time.current.strftime("%U")
-        shows << schedule.events.order('start_time ASC')
-      end
-    end
-    shows.flatten
-  end
-
-  def thursday
-    shows = []
-
-    self.schedules.each do |schedule|
-      if schedule.date.wday == 4 && schedule.date.strftime("%U")== Time.current.strftime("%U")
-        shows << schedule.events.order('start_time ASC')
-      end
-    end
-    shows.flatten
-  end
-
-  def friday
-    shows = []
-
-    self.schedules.each do |schedule|
-      if schedule.date.wday == 5 && schedule.date.strftime("%U")== Time.current.strftime("%U")
-        shows << schedule.events.order('start_time ASC')
-      end
-    end
-    shows.flatten
-  end
-  def saturday
-    shows = []
-
-    self.schedules.each do |schedule|
-      if schedule.date.wday == 6 && schedule.date.strftime("%U")== Time.current.strftime("%U")
-        shows << schedule.events.order('start_time ASC')
-      end
-    end
-    shows.flatten
-  end
-
-  def sunday
-    shows = []
-
-    self.schedules.each do |schedule|
-      if schedule.date.cwday == 7 && schedule.date.strftime("%W") == Time.current.strftime("%W")
-        shows << schedule.events.order('start_time ASC')
-      end
-    end
-    shows.flatten
-  end
 
 end
