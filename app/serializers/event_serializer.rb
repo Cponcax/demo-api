9328708b0b9db9a8c0cee3be3dfbf4d3cc43f554
Alex Ponce <alex.ponce@503estudio.com>
2016-04-06 22:31:02 -0600

    add streaming_url in event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,6 +1,10 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id,  :channel_id,  :name, :cover ,:logo ,:rating, :datetime
+  attributes :id,  :channel_id,  :name, :cover ,:logo ,:rating, :datetime, :streaming_url
 
+  def streaming_url
+    object.show.streaming_url
+  end
+  
   def name
     object.show.name
   end
