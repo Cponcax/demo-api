17922a32d85a474f5d0e2fb0fe6dc92752778c1a
Alex Ponce <alex.ponce@503estudio.com>
2016-04-07 04:39:55 -0600

    Add accestoken in sing_up

---
+++ app/serializers/access_token_serializer.rb
@@ -0,0 +1,19 @@
+class AccessTokenSerializer < ActiveModel::Serializer
+  attributes :access_token, :token_type, :expires_in, :refresh_token, :scope, :created_at
+
+  def access_token
+    object.token
+  end
+
+  def token_type
+    'bearer'
+  end
+
+  def scope
+    'write'
+  end
+
+  def created_at
+    object.created_at.to_i
+  end
+end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,11 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name, :email, :gender, :birth_date, :bio, :token
-  def token
-    # puts "self: " + self.inspect
-    # puts "self object: " + self.object.inspect
-    #{self.object.access_tokens}
-  
-
-  end
+  attributes :id, :username, :first_name, :last_name, :email, :gender, :birth_date, :bio
+  has_many :access_tokens
 
 end
