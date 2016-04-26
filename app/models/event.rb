7fa465f6a790ed9e17e6fdb361838ddd737e5716
Alex Ponce <alex.ponce@503estudio.com>
2016-04-26 15:01:21 -0600

    Add changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,10 +5,8 @@ class Event < ActiveRecord::Base
 
   validates :start_time, :end_time, :streaming_url ,presence: true
 
-
   def self.hour
     t = Time.utc(2001, 1, 1, Time.current.hour, Time.current.min, Time.current.sec)
-
     e = Schedule.get_day.events
     e.where("? BETWEEN start_time AND end_time", t )
   end
