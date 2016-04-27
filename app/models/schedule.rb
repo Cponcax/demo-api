4b08affa3d0150f98832cbb718be62ba7f1c81f4
Alex Ponce <alex.ponce@503estudio.com>
2016-04-26 23:23:14 -0600

    Add changes

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -8,8 +8,11 @@ class Schedule < ActiveRecord::Base
  
  def self.get_day
   Time.zone = "Central America"
-  t = Time.utc(Time.current.year, Time.current.month, Time.current.day, Time.current.hour, Time.current.min, Time.current.sec).in_time_zone
+
+  t = Time.current.in_time_zone
+
   puts "TIEMPO WITH TIME ZONE::" + t.inspect
+
   find_by("date =?", t)
  end
 
