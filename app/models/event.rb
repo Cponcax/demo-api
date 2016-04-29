e4f0c2f291c60b243402413766dca48e8b6ac61b
Alex Ponce <alex.ponce@503estudio.com>
2016-04-29 13:03:29 -0600

    Add changes to shows live

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,10 +7,28 @@ class Event < ActiveRecord::Base
 
   validates :start_time, :end_time, :streaming_url ,presence: true
 
+
+
   def self.get_show_live
    # Time.zone = "Central America"
- 
+    
+   #  t = Time.utc(2001, 1, 1, Time.current.hour, Time.current.min, Time.current.sec)
+   #  e = Schedule.get_day.events
 
+   #  t = Time.current.in_time_zone.strftime('%H:%M:%S')
+   #  puts "TIME CURRENT: " + t.inspect
+
+   #  events = Schedule.get_day.events
+
+   #  #where("? BETWEEN start_time AND end_time", t )
+   #  events.select { |event| 
+   #    st = event.start_time.in_time_zone.strftime('%H:%M:%S')
+   #    puts "START TIME: " + st.inspect
+   #    et = event.end_time.in_time_zone.strftime('%H:%M:%S')
+   #    puts "END TIME: " + et.inspect
+        
+   #      t >= st && t < et
+   #  }
   puts "TIME CURRENT::" + Time.current.inspect
 
   puts"==============" 
@@ -19,26 +37,22 @@ class Event < ActiveRecord::Base
   
   t = Time.current.utc.to_time_of_day
 
-  events = Schedule.get_day  rescue []
+  events = Schedule.get_schedules_per_day rescue []
 
-  show_live = events.map {|e| e.events[0]} rescue []
+    show_live = events.map {|e| e.events} rescue []
 
-  puts "EVENTS" + events.inspect
-
-  show_live.select{ |e| 
-    begin
-      st = e.start_time.to_time_of_day 
-    puts "ST::" + st.inspect
-    et = e.end_time.to_time_of_day 
-    puts "ET::" + et.inspect
-    Tod::Shift.new(st, et).include?(t)
-    rescue Exception => e
-      
-    end
-    
- 
-  } 
+  #puts "EVENTS" + events.inspect
    
+  s = show_live.select { |event|  
+    st = event.start_time.to_time_of_day rescue []
+    et = event.end_time.to_time_of_day rescue []
+   
+    Tod::Shift.new(st, et).include?(t) rescue []
+    
+    }
+
+  s.flatten
+  
   end
 
 end
