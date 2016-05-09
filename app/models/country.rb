a115e70d2f300d94f43b6e67b7d9c222f382d41c
Alex Ponce <alex.ponce@503estudio.com>
2016-05-09 09:52:08 -0600

    Add seed to country and remove relationship country with user

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,7 @@
 class Country < ActiveRecord::Base
-  #has_many :users
+ 
   has_many :event_countries 
   has_many :events, through: :event_countries
   
-  validates :name, :alpha2, :alpha3, presence: true, uniqueness: true
+  validates :name, :alpha2, presence: true, uniqueness: true
 end
