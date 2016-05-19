1c56b6cd7e8a84bf739aa4a9a0c4688527341482
Rafael <rafael.valencia@503estudio.com>
2016-05-19 16:51:38 -0600

    Add class variable with payment information.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,26 @@
 class Subscription < ActiveRecord::Base
   belongs_to  :plan
   belongs_to  :customer
+
+  # Initialize the payment object
+  @@payment ||= {
+    "intent" =>  "sale",
+    "payer" =>  {
+      "payment_method" =>  "paypal" 
+    },
+    "transactions" => [ 
+      {
+        "amount" => {
+          "total" =>  "15.00",
+          "currency" =>  "USD" 
+        },
+      "description" =>  "This is the payment transaction description." 
+      } 
+    ] 
+  }
+
+  def makePayment metadata_id
+    # metadata_id from mobile sdk
+    correlation_id = metadata_id
+  end
 end
