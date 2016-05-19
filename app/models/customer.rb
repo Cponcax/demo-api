9461c8ce007052228b360a0edc8f56b9f8a5ede0
Rafael <rafael.valencia@503estudio.com>
2016-05-19 16:15:02 -0600

    Add exch_token method in customer model to get access_token from PayPal.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,33 @@
+require 'paypal-sdk-rest'
+
+include PayPal::SDK::REST::DataTypes
+include PayPal::SDK::Core::Logging
+include PayPal::SDK::OpenIDConnect
+
 class Customer < ActiveRecord::Base
   belongs_to  :user
   
   #for sdk paypal
   has_many    :tokens
-  
+
   has_many    :subscriptions
   has_many    :invoices
+
+
+  def exch_token authorization_code
+    begin
+      # authorization code from mobile sdk
+
+      # exchange authorization code with refresh/access token
+      logger.info "Exchange authorization code with refresh/access token"
+      info  = ::FuturePayment.exch_token(authorization_code)
+
+      # get access_token, refresh_token from tokeninfo. refresh_token can be exchanged with access token. See https://github.com/paypal/PayPal-Ruby-SDK#openidconnect-samples
+      logger.info "Successfully retrieved access_token=#{info.access_token} refresh_token=#{tokeninfo.refresh_token}"
+
+      tokens.create!(tokeninfo)      
+    rescue Exception => e
+      puts "Error: {e.message}"
+    end
+  end
 end
