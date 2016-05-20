4627dfc57834b7e10909c3db628ba09c2339dcaa
Alex Ponce <alex.ponce@503estudio.com>
2016-05-20 09:42:20 -0600

    Add changes in credentials to paypal

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,13 +1,13 @@
-PayPal::SDK.load("config/paypal.yml", Rails.env)
+#PayPal::SDK.load("config/paypal.yml", Rails.env)
 PayPal::SDK.logger = Rails.logger
 
-# PayPal::SDK.configure(
-#   client_id: ENV["client_id"],
-#   client_secret: ENV["client_secret"],
-#   username: ENV["username"],
-#   password: ENV["password"],
-#   signature: ENV["signature"],
-#   app_id: ENV["app_id"],
-#   mode: ENV["mode"],
-#   http_timeout: 30 
-# )
+PayPal::SDK.configure(
+  client_id: ENV["client_id"],
+  client_secret: ENV["client_secret"],
+  username: ENV["username"],
+  password: ENV["password"],
+  signature: ENV["signature"],
+  app_id: ENV["app_id"],
+  mode: ENV["mode"],
+  http_timeout: 30 
+)
