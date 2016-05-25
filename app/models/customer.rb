ddd02c8d6a7908a2ad36baff248b635dbd53b090
Rafael <rafael.valencia@503estudio.com>
2016-05-25 10:22:28 -0600

    Change in renew_subscription method.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -54,7 +54,7 @@ class Customer < ActiveRecord::Base
     s = subscriptions.find_or_initialize_by ended_at: nil
         
     unless s.new_record?
-      s.current_period_end + 30.days 
+      s.current_period_end = s.current_period_end + 5.minutes
     end
     s.status = :active 
         
